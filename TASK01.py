1a083585164cd45751b72d2023f8671762076121
Strxngs <euniceho1111@gmail.com>
2026-03-06 11:20:52 +0800

    Update TASK01.py

--- TASK01.py
+++ TASK01.py
@@ -6,7 +6,19 @@ with open("todo.txt", "w") as file:
 	file.write("eat")
 
 with open("todo.txt", "r") as file:
+	lines = file.readlines()
+	for i, lines in enumerate(range(lines,1)):
+		print(f"{i}). {lines.strip()}")
+
+print("Add item pls")
+
+with open("todo.txt", "a") as file:
+	file.write(input("what do you want to do?"))
+
+with open("todo.txt", "r") as file:
 	content = file.read()
+	print("Updated List:")
 	print(content)
 	
 	
+
