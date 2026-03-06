a655ff91b7bfd0d4be81180a9967b870c8b572dd
Strxngs <euniceho1111@gmail.com>
2026-03-06 11:40:06 +0800

    Create TASK02.py

---
+++ TASK02.py
@@ -0,0 +1,20 @@
+import json 
+
+student_manager = {
+	"students" : [
+		{"name": "Alice", "age": 20, "grades": [85, 90, 88], "email": "87z9@gmail.com"},
+		{"name": "Bob", "age": 19, "grades": [78, 82, 80], "email": "x78y@yahoo.com"},
+	    {"name": "Charlie", "age": 21, "grades": [92, 95, 90], "email": "98a7@outlook.com"}
+  	]
+}
+
+with open("students.json", "w") as file:
+	json.dump(student_manager, file, indent =4)
+
+with open("students.json", "r") as file:
+	data = json.load(file)
+	for student in data["students"]:
+		grades = student["grades"]
+		average = sum(grades)/len(grades)
+		print(f"student["name"] average: {average}")
+
