985f0d3c777ac5cf568da6ae85d082c26a550382
Strxngs <euniceho1111@gmail.com>
2026-03-06 12:10:04 +0800

    Update TASK02.py

--- TASK02.py
+++ TASK02.py
@@ -11,22 +11,32 @@ student_manager = {
 with open("students.json", "w") as file:
 	json.dump(student_manager, file, indent =4)
 
+#find average
 with open("students.json", "r") as file:
 	average_grades = []
 	data = json.load(file)
 	for student in data["students"]:
 		grades = student["grades"]
 		average = sum(grades)/len(grades)
-		average.append(average_grades)
-		print(f"student["name"] average: {average}")
+		average_grades.append(average)
+		print(f"{student['name']} average: {average}")
 	highest_average = max(average_grades)
-	print(f"{student["name"] has the highest average of {highest_average}!")
+	highest_student = ""
+	for name, avg in student_avg_map.items():
+	    if avg == highest_average:
+	        highest_student = name
+	        break  
+	print(f"{student["name"]} has the highest average of {highest_average}!")
 
-#update score 
+ 
 for student in data["students"]:
-	if student["name"] == "Charlie":
-		student["grades"][1] = 93
-print(students_manager)
+	if student['name'] == "Charlie":
+		student['grades'][1] = 93
+		
+#update score
+with open("students.json", "w") as file:
+	json.dumps(data, file, indent=4)
+print(student_manager)
 
 
 	
