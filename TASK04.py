8c3fa572cd7c37a34531ec8e2d5ee1e343afe52e
Strxngs <euniceho1111@gmail.com>
2026-03-13 12:13:14 +0800

    Create TASK04.py
    not doneee

---
+++ TASK04.py
@@ -0,0 +1,22 @@
+import json
+import csv
+
+#write data.txt
+with open("data.txt", "w") as file:
+    file.write("Hello, World!\n")
+    file.write("This is a sample text file.\n")
+    file.write("It contains multiple lines of text.\n")
+
+#read data.txt
+with open("data.txt", "r") as file:
+    content = file.read()
+    print(content)
+
+#convert to json
+with open("data.json", "w") as file:
+    json.dump(content, file, indent=4)
+
+#convert to csv
+with open("data.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerows(content)
