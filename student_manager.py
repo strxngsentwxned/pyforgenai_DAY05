67bcc989068ae268750346274c74c23233bb6b64
Strxngs <euniceho1111@gmail.com>
2026-03-16 11:34:41 +0000

    create student_manager.py

---
+++ student_manager.py
@@ -0,0 +1,14 @@
+import json
+import csv
+import os
+
+def menu():
+    print("Menu:")
+    print("1. Add student")
+    print("2. View all students")
+    print("3. Calculate statistics")
+    print("4. Search student")
+    print("5. Update student scores")
+    print("6. Export to JSON")
+    print("7. Export to CSV")
+    print("8. Exit")
